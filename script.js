3cf4e4f574751a690a134efdbf9fca286e4f5b3a
Pepper <pandapup3000@gmail.com>
2025-04-24 20:30:51 -0500

    fixed bugs form last commit

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-hello="hello,I'm yummy pizza"
+hello = "hello,I'm yummy pizza"
 console.log(hello)
 
-<form id="myForm">
-  <input type="text" name="username" required>
+
+const form = document.getElementById("myForm");
+
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Form submitted!");
+});
+
+const usernameInput = document.getElementById("username");
+usernameInput.value = "Default Value";
+
+const submitButton = document.getElementById("submitBtn");
+submitButton.addEventListener("click", function () {
+  alert(`Entered username: ${usernameInput.value}`);
+});
+
+
+<form id="myForm"><input type="text" name="username"/> require&gt;
   <button type="submit">Submit</button>
 </form>;
-<script>< src="https://code.jquery.com/jquery-3.6.0.min.js"></script>;
-$('#myForm').on('submit', function(event){
-console.log('Form submitted!');
+<script> "src"="https://code.jquery.com/jquery-3.6.0.min.js"</script>;
+$('#myForm').on('submit', function (event) {
+  console.log('Form submitted!');
 });
